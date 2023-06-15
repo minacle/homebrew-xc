5a10b83778e563e67c41b047c2e84bc5318ed11b
Mina Her <minacle@live.com>
2023-06-15 17:30:25 +0900

    xc 0.13.1

--- Formula/xc.rb
+++ Formula/xc.rb
@@ -1,8 +1,8 @@
 class Xc < Formula
   desc "Best command to run Xcode app what you want"
   homepage "https://github.com/minacle/xc"
-  url "https://github.com/minacle/xc/archive/refs/tags/v0.13.0.tar.gz"
-  sha256 "c2f8a23d3fab2725370133e46355f4dcf602dd77f2d077ac9539584a72177f3d"
+  url "https://github.com/minacle/xc/archive/refs/tags/v0.13.1.tar.gz"
+  sha256 "4eb9265c7d9a9f85213f1f8afbd8aac24aa3adbcc51a9a3fbb4a3e70157ebd6e"
   license "Unlicense"
 
   depends_on xcode: ["14.0", :build]
